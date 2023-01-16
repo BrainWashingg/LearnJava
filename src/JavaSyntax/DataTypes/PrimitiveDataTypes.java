134ef223acc549d492fb433e2fd963039d732fec
BrainWashingg <antuhov33rus@gmail.com>
2023-01-16 07:49:25 +0300

    типы данных

---
+++ src/JavaSyntax/DataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,14 @@
+package JavaSyntax.DataTypes;
+
+public class PrimitiveDataTypes {
+/*
+    Целые числа со знаком:    byte  {-128;127}|{2^7-1;2^7}        8 bit
+                              short {-32768;32767}|{2^15-1;2^15}  16 bit
+                              int   {2^31-1;2^31}                 32 bit
+                              long  {2^63-1;2^63}                 64 bit
+    Числа с плавающей точкой: float  {1,4e-045;3,4e+038}          32 bit
+                              double {4,9e-324;1,8e+308}          64 bit
+    Символы:                  char  {0;2^16}                      16 bit
+    Логическое значение:      boolean {false;true}                32 bit
+    */
+}
--- src/JavaSyntax/HelloWorld.java
+++ src/JavaSyntax/HelloWorld.java
@@ -1,7 +1,33 @@
 package JavaSyntax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello my world");
+
+        Scanner in = new Scanner(System.in);
+        Long num = in.nextLong();
+
+        Long hours = 0L, minutes = 0L, seconds = 0L;
+
+
+        hours = num / 3600;
+        num = num % 3600;
+
+        minutes = num / 60;
+        seconds = minutes % 60;
+
+        String h = new String(String.valueOf(hours));
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+        System.out.println(h);
+        Date date = new Date(3600L);
+        System.out.println(date);
     }
+
 }
+
